fed872d10a7f48226a61850db96520471e7268b2
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-10-24 15:59:22 -0500

    incomes and expenses feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import TransactionForm from "./components/transactions/TransactionForm";
 import { GlobalProvider } from "./context/GlobalState";
 import TransactionList from "./components/transactions/TransactionList";
+import IncomesExpenses from "./components/IncomesExpenses";
 
 const App = () => {
   return (
     <GlobalProvider>
       <Header />
+      <IncomesExpenses/>
       <Balance />
       <TransactionForm />
       <TransactionList />
---
+++ src/components/IncomesExpenses.jsx
@@ -0,0 +1,32 @@
+import { useGlobalState } from "../context/GlobalState";
+
+const IncomesExpenses = () => {
+
+const {transactions} = useGlobalState();
+
+const amounts = transactions.map(transaction => transaction.amount);
+
+const incomes = amounts
+  .filter(item => item > 0)
+  .reduce((acc, item) => (acc+=item),0);
+
+const expenses = amounts
+  .filter(item => item < 0)
+  .reduce((acc, item) => (acc+=item),0) * -1;
+
+
+  return (
+    <>
+      <div>
+        <h4>Incomes</h4>
+        <p>{incomes}</p>
+      </div>
+      <div>
+        <h4>Expenses</h4>
+        <p>{expenses}</p>
+      </div>
+    </>
+  );
+};
+
+export default IncomesExpenses;
